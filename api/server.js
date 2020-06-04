6ec78d275116f381498754dfe7e99798d7ef035d
Alex Magana <46731475+jamcoding@users.noreply.github.com>
2020-06-03 22:57:20 -0500

    added session to db

--- api/server.js
+++ api/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
 const session = require('express-session');
+const knexSessionStore = require('connect-session-knex')(session);
 
 const sessionConfig = {
     name: 'amsession',
@@ -12,7 +13,15 @@ const sessionConfig = {
         httpOnly: true,
     },
     resave: false,
-    saveUninitialized: false
+    saveUninitialized: false,
+
+    store: new knexSessionStore({
+        knex: require('../database/dbConfig.js'),
+        tablename: "sessions",
+        sidfieldname: "sid",
+        createtable: true,
+        clearInterval: 1000 * 60 * 60
+    })
 };
 
 const usersRouter = require('../users/users-router.js');
